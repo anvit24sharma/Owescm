0ff15ccfe4866c65d32a9b9898d2f8f8d7703786
MyCity <mycity1001@gmail.com>
2020-03-22 23:40:04 +0530

    Login

---
+++ app/src/main/java/com/owescm/client/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.owescm.client
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        txt_SignUp.setOnClickListener {
+            startActivity((Intent(this, SignUpActivity::class.java)))
+            finish()
+        }
+        btn_login.setOnClickListener {
+            startActivity((Intent(this, MainActivity::class.java)))
+
+        }
+
+    }
+}
