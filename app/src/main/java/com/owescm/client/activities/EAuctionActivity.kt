4bcb5288ca4b7ecc5bd958a3140dcdaf25b6fca7
MyCity <mycity1001@gmail.com>
2020-04-17 23:29:17 +0530

    erfx new invite suppliers

--- app/src/main/java/com/owescm/client/activities/EAuctionActivity.kt
+++ app/src/main/java/com/owescm/client/activities/EAuctionActivity.kt
@@ -2,14 +2,58 @@ package com.owescm.client.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.owescm.client.Fragment.EAuctionFragment.EAuctionClosedFragment
+import com.owescm.client.Fragment.EAuctionFragment.EAuctionTodayFragment
+import com.owescm.client.Fragment.EAuctionFragment.EAuctionUpcomingFragment
 import com.owescm.client.R
+import kotlinx.android.synthetic.main.activity_e_auction.*
 
 class EAuctionActivity : AppCompatActivity() {
-
+    val eAuctionFragment = EAuctionTodayFragment()
+    val eAuctionTodayFragment = EAuctionTodayFragment()
+    val eAuctionUpcomingFragment = EAuctionUpcomingFragment()
+    val eAuctionClosedFragment = EAuctionClosedFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_e_auction)
+        openEAuctionFragment()
+        rl_EAuction_today.setOnClickListener {
+            openEAuctionTodayFragment()
+        }
+        rl_EAuction_upcoming.setOnClickListener {
+            openEAuctionUpcomingFragment()
+        }
+        rl_EAuction_closed.setOnClickListener {
+            openEAuctionClosedFragment()
+        }
 
 
+    }
+
+    private fun openEAuctionClosedFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout_EAuction, eAuctionClosedFragment)
+        transaction.commit()
+
+    }
+
+    private fun openEAuctionUpcomingFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout_EAuction, eAuctionUpcomingFragment)
+        transaction.commit()
+
+    }
+
+    private fun openEAuctionTodayFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout_EAuction, eAuctionTodayFragment)
+        transaction.commit()
+
+    }
+
+    private fun openEAuctionFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout_EAuction, eAuctionFragment)
+        transaction.commit()
     }
 }
